14b1be259e7755a9c6208f43c8d395e20c8dea5e
Noah Laux <noahlaux@gmail.com>
2012-07-31 10:22:07 +0300

    Update js/notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 	var xlsPath			= localStorage.getItem('xslPath'),
-		xls				= lib.parseXML( localStorage.getItem( xlsPath ) ),
-		notifications	= lib.parseXML( localStorage.getItem( 'notifications' ) ),
+		xls				= lib.parseXML( localStorage.getItem(xlsPath) ),
+		notifications	= lib.parseXML( localStorage.getItem('notifications') ),
 		html			= lib.transformXML( xls, notifications );
 
 	document.getElementById('container').appendChild( html );
 
 	// Open options
-    document.getElementById('options').addEventListener( 'click', function( e ) {
-        chrome.tabs.create({
-            url: 'options.html'
-        });
-    });
+	document.getElementById('options').addEventListener( 'click', function( e ) {
+		chrome.tabs.create({
+		    url: 'options.html'
+		});
+	});
 	
 });
